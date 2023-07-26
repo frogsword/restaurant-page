3ae692f949255b0579180830f9e835927e215ae7
frogsword <cbheavin@gmail.com>
2023-07-26 06:20:23 -0500

    added menu

--- src/pages/Menu.js
+++ src/pages/Menu.js
@@ -4,11 +4,45 @@ const Menu = (() => {
     mainMenu.classList.add('mainMenu');
 
     mainMenu.innerHTML = 
-    `<h1>Comes in full stack (3 cakes) or short stack (2 cakes).</h1>
-    <div class='menu-item'>Buttermilk Pancake</div>
-    <div class='menu-item'>Chocolate Pancake</div>
-    <div class='menu-item'>Blueberry Pancake</div>
-    <div class='menu-item'>Strawberry Shortcake Pancake</div>`
+    `
+        <h1>Pancakes</h1>
+        <div class='menu-item'>
+            Buttermilk 
+            <div class="menu-item-price">$7.99</div>
+        </div>
+        <div class='menu-item'>
+            Chocolate Chip
+            <div class="menu-item-price">$7.99</div>
+        </div>
+        <div class='menu-item'>
+            Peanut Butter
+            <div class="menu-item-price">$7.99</div>
+        </div>
+        <div class='menu-item'>
+            Birthday Cake
+            <div class="menu-item-price">$8.49</div>
+        </div>
+        <div class='menu-item'>
+            Cinnamon Bun
+            <div class="menu-item-price">$8.49</div>
+        </div>
+        <div class='menu-item'>
+            Pecan
+            <div class="menu-item-price">$7.99</div>
+        </div>
+        <div class='menu-item'>
+            Blueberry
+            <div class="menu-item-price">$8.49</div>
+        </div>
+        <div class='menu-item'>
+            Strawberry
+            <div class="menu-item-price">$8.49</div>
+        </div>
+        <div class='menu-item' id="last-item">
+            Protein
+            <div class="menu-item-price">$9.49</div>
+        </div>
+    `
 
     content.appendChild(mainMenu);
 
